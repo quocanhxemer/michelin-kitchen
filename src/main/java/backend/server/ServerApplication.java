a40a6cbdb447f4904856c831b2c5cee03c2cdae6
kyredyrf nipohc <quocanhxemer@gmail.com>
2024-09-09 19:07:14 +0200

    rename package names

--- src/main/java/backend/server/ServerApplication.java
+++ src/main/java/backend/server/ServerApplication.java
@@ -1,13 +1,13 @@
-package backend;
+package backend.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 
 }
--- src/test/java/backend/server/ServerApplicationTests.java
+++ src/test/java/backend/server/ServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.backend;
+package backend.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class ServerApplicationTests {
 
 	@Test
 	void contextLoads() {
